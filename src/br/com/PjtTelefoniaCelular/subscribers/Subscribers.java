3e15046fba0559daa01e4a3a6a1c3c4bcbd2f78f
Iago Viana <iago63803@gmail.com>
2023-11-08 20:54:08 -0300

    feat: add attributes and methods for the subscribers class

--- src/br/com/PjtTelefoniaCelular/subscribers/Subscribers.java
+++ src/br/com/PjtTelefoniaCelular/subscribers/Subscribers.java
@@ -1,10 +1,43 @@
 package br.com.PjtTelefoniaCelular.subscribers;
 
+import call.Call;
+
 public class Subscribers {
-   private int cpf;
+   private long cpf;
    private String name;
    private int celNumber;
+   private int numOfCalls;
+
+   private Call[] calls;
+
+   public Subscribers(long cpf, String name, int celNumber, int numOfCalls) {
+      this.cpf = cpf;
+      this.name = name;
+      this.celNumber = celNumber;
+      this.calls = new Call[numOfCalls];
+      this.numOfCalls = 0;
+   }
+
+   public Long getCpf() {
+      return cpf;
+   }
+
+   @Override
+   public String toString() {
+      return "CPF: " + cpf + "\nNome: " + name + "\nNúmero: " + celNumber + "\nNúmero de Chamadas: " + numOfCalls;
+   }
+
+
+
+
+
+
 
 
 }
+
+
+
+
+
 
