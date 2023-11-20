3461a5944f4a9da1c6d6349afb10e9582e34bfd1
Iago Viana <iago63803@gmail.com>
2023-11-20 10:48:18 -0300

    fix duplicated classes

---
+++ src/br/com/PjtTelefoniaCelular/call/Call.java
@@ -0,0 +1,31 @@
+package br.com.PjtTelefoniaCelular.call;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class Call {
+    private GregorianCalendar date;
+    private int duration;
+
+    // Construtor
+    public Call(GregorianCalendar date, int duracao) {
+        this.date = date;
+        this.duration = duracao;
+    }
+
+    // Método para obter a data
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    // Método para obter a duração
+    public int getDuracao() {
+        return duration;
+    }
+
+    // Método para obter uma representação textual da chamada
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return "Data: " + sdf.format(date.getTime()) + "\nDuração: " + duration + " minutos";
+    }
+}
