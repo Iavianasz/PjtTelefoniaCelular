e5ffe1a413f46d45da58737ea41f27791887517b
Iago Viana <iago63803@gmail.com>
2023-11-24 13:21:58 -0300

    feat: add recharge class

---
+++ src/br/com/PjtTelefoniaCelular/recharge/Recharge.java
@@ -0,0 +1,30 @@
+package br.com.PjtTelefoniaCelular.recharge;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class Recharge {
+    private GregorianCalendar date;
+    private float value;
+
+    //Construtor da Classe
+    public Recharge(GregorianCalendar date, float value) {
+        this.date = date;
+        this.value = value;
+    }
+
+    // getters e setters
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    // Método toString para representação textual
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return "Data: " + sdf.format(date.getTime()) + "\nValor: R$ " + value;
+    }
+}
